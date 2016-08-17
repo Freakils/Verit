1c464e0270112e94b2157e8b316d2497c52fc39c
Freakils <luvo@projectcodex.co>
2016-08-17 15:15:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var expressHandlebars = require('express-handlebars');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static('public'));
+
+//setup handlebars
+app.engine('hbs', expressHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res){
+    res.render('light');
+});
+
+app.get('/transaction', function(req, res){
+  res.render('transaction');
+});
+
+var port = process.env.port || 3007;
+http.listen(port, function(){
+    console.log('running at port :' , port)
+});
